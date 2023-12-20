5221dffe1f7b97bc8e729515667b12c86335a158
yung78 <alexdrum@mail.ru>
2023-12-20 16:07:32 +0300

    re Done

--- src/saga/index.js
+++ src/saga/index.js
@@ -2,20 +2,19 @@ import { takeLatest, put, spawn, debounce, retry } from 'redux-saga/effects';
 import { changeSearchField, skillsRequest, skillsSuccess, skillsFailur, resetState } from '../app/searchSlice';
 import { searchSkills } from '../app/searchSkills';
 
-function filterChangeSearchAction({ type, payload }) {
-  console.log(payload)
-  return type === changeSearchField.type && payload.trim() !== ''
-}
-
 // watcher_1
 function* watchChangeSearchSaga() {
-  yield debounce(300, filterChangeSearchAction, handleChangeSearchSaga);
+  yield debounce(300, changeSearchField.type, handleChangeSearchSaga);
 }
 
 // worker_1
 function* handleChangeSearchSaga(action) {
-  console.log('worker_1')
-  yield put(skillsRequest(action.payload));
+  if (action.payload) {
+    yield put(skillsRequest(action.payload));
+  } else {
+    console.log('reset');
+    yield put(resetState());
+  }
 }
 
 // watcher_2
@@ -29,28 +28,11 @@ function* handleSearchSkillsSaga(action) {
     const data = yield retry(3, 1000, searchSkills, action.payload);
     yield put(skillsSuccess(data));
   } catch(err) {
-    yield put(skillsFailur(err.message))
+    yield put(skillsFailur(err.message));
   }
-}
-
-
-function filterResetAction({ type, payload }) {
-  return type === changeSearchField.type && payload.trim() === ''
-}
-
-// watcer_0(home work)
-function* watchResetSaga() {
-  yield takeLatest(filterResetAction, handleResetSaga); //debounce(500, filterResetAction, handleResetSaga) - не помогло
-}
-
-// watcer_0(home work)
-function* handleResetSaga() {
-  console.log('reset');
-  yield put(resetState());
 }
 
 export default function* saga() {
   yield spawn(watchChangeSearchSaga);
   yield spawn(watchSearchSkillsSaga);
-  yield spawn(watchResetSaga);
 }
